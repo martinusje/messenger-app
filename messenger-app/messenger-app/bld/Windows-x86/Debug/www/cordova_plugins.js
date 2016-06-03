cd38f0c14cc7dc8b5f304b4f040e878a9acfab3c
LucaHodes <lhodes@unic-utrecht.nl>
2016-06-03 15:13:24 +0200

    uiterlijk gemaakt

---
+++ messenger-app/messenger-app/bld/Windows-x86/Debug/www/cordova_plugins.js
@@ -0,0 +1,35 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/windows/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
+    },
+    {
+        "file": "plugins/ionic-plugin-keyboard/src/windows/KeyboardProxy.js",
+        "id": "ionic-plugin-keyboard.KeyboardProxy",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-statusbar": "2.1.1",
+    "ionic-plugin-keyboard": "2.0.1"
+}
+// BOTTOM OF METADATA
+});
